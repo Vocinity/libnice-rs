998d8b1b0a5344d860d47582ed467ec80fa0e638
Abdul Rehman <abdul@vocinity.com>
2020-12-09 02:07:16 +0500

    feat: Generate basic Nice bindings

---
+++ nice/src/lib.rs
@@ -0,0 +1,19 @@
+pub use ffi;
+
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+#[allow(clippy::unreadable_literal)]
+#[allow(clippy::too_many_arguments)]
+#[allow(clippy::match_same_arms)]
+mod auto;
+pub use crate::auto::*;
+
+pub mod prelude {
+    pub use crate::auto::traits::*;
+}
---
+++ nice/sys/src/manual.rs
@@ -0,0 +1,26 @@
+impl ::std::fmt::Debug for sockaddr {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        f.debug_struct(&format!("sockaddr @ {:?}", self as *const _))
+         .finish()
+    }
+}
+#[derive(Copy, Clone)]
+pub struct sockaddr(libc::sockaddr);
+
+impl ::std::fmt::Debug for sockaddr_in {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        f.debug_struct(&format!("sockaddr_in @ {:?}", self as *const _))
+         .finish()
+    }
+}
+#[derive(Copy, Clone)]
+pub struct sockaddr_in(libc::sockaddr_in);
+
+impl ::std::fmt::Debug for sockaddr_in6 {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        f.debug_struct(&format!("sockaddr_in6 @ {:?}", self as *const _))
+         .finish()
+    }
+}
+#[derive(Copy, Clone)]
+pub struct sockaddr_in6(libc::sockaddr_in6);
